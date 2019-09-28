b91d5b1d2a2ae7f6deff95cacc5028f5c3aeca2b
wrt2sanjoy <37881390+wrt2sanjoy@users.noreply.github.com>
2019-09-29 03:09:59 +0530

    Strong Password HackerRank Problem
    Strong Password
    Jugnu the little girl came to know about a social networking site called Friends-Book. To remain in contact
    with her friends even after graduation, she decided to join the networking site. The signup page required
    her to input a name and a password. However, the password must be strong. The website considers a
    password to be strong if it satisfies the following criteria:
    Its length is at least .
    It contains at least one digit.
    It contains at least one lowercase English character.
    It contains at least one uppercase English character.
    It contains at least one special character. The special characters are: !@#$%^&*()-+
    She typed a random string of length in the password field but wasn't sure if it was strong. Given the
    string she typed, can you find the minimum number of characters she must add to make her password
    strong?
    Note: Here's the set of types of characters in a form you can paste in your solution:
    numbers = "0123456789"
    lower_case = "abcdefghijklmnopqrstuvwxyz"
    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
    special_characters = "!@#$%^&*()-+"
    Input Format
    The first line contains an integer denoting the length of the string.
    The second line contains a string consisting of characters, the password typed by Jugnu. Each character
    is either a lowercase/uppercase English alphabet, a digit, or a special character.
    Constraints
    Output Format
    Print a single line containing a single integer denoting the answer to the problem.
    Sample Input 0
    3
    Ab1
    Sample Output 0
    3
    Explanation 0
    She can make the password strong by adding characters, for example, $hk , turning the password into
    Ab1$hk which is strong.
    characters aren't enough since the length must be at least .
    Sample Input 1
    11
    #HackerRank
    Sample Output 1
    1
    Explanation 1
    The password isn't strong, but she can make it strong by adding a single digit.

---
+++ Solution.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumNumber function below.
+   public static void main(String[] args) {
+        // Return the minimum number of characters to make the password strong
+              int ans =0;
+			  Scanner sc=new Scanner(System.in);
+               String password=sc.nextLine();
+			   int n=password.length();
+			  //int count =0;
+               boolean small=false,caps=false,numeric=false,special=false;
+				   // System.out.println("else");
+                   if(password.length()>=6)
+                   {
+					   for(int i=0;i<password.length();i++)
+                         {
+					 
+                       if(password.charAt(i)>=48 && password.charAt(i)<=57 )
+							 
+						        small=true;
+							 
+
+						
+						if   (password.charAt(i)>=65 && password.charAt(i)<=90 ) 
+
+								 caps=true;
+							 
+
+						   if (password.charAt(i)>=97 && password.charAt(i)<=122)
+						   		 numeric=true;
+						   
+
+
+						   if(password.charAt(i)=='!' || password.charAt(i)=='@' ||                                                      password.charAt(i)=='#' || password.charAt(i)=='$' ||                                                      password.charAt(i)=='%' || password.charAt(i)=='^' || 
+                               password.charAt(i)=='&'  || password.charAt(i)=='*'|| 
+                               password.charAt(i)=='(' || password.charAt(i)==')' || 
+                               password.charAt(i)=='-' || password.charAt(i)=='+')
+						  
+							  special=true;
+							
+                    
+					  }
+					  if(small==false)
+						  ans++;
+                      if(caps==false)
+						  ans++;
+                      if(numeric==false) 
+						  ans++;
+                      if(special==false) 
+						  ans++;
+
+                   }
+				   	  
+                  //  else
+                   //{
+                     //  ans=6-password.length();
+                   //}
+                   
+
+                  System.out.println(ans);
+                  
+                  
+    }
+
+   
+}
